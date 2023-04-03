6a41b2fd6f638b87227c0125e9af9906d3ca9ac4
kusi <lawcubegsm@gmail.com>
2023-04-03 16:12:53 +0000

    Add user test descriptions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it "is valid with valid attributes"
+   it "is not valid without a email"
+   it "is not valid with an invalid email"
+   it "is not valid without a password"
+   it "is not valid with password less than 6"
+   it "is not valid without a role"
 end
