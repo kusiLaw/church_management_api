634f11b44fc97a7cb84fbe70a3998e017431dbb7
kusi <lawcubegsm@gmail.com>
2023-04-03 16:21:10 +0000

    Add linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   it "is valid with valid attributes"
-   it "is not valid without a email"
-   it "is not valid with an invalid email"
-   it "is not valid without a password"
-   it "is not valid with password less than 6"
-   it "is not valid without a role"
+  it 'is valid with valid attributes'
+  it 'is not valid without a email'
+  it 'is not valid with an invalid email'
+  it 'is not valid without a password'
+  it 'is not valid with password less than 6'
+  it 'is not valid without a role'
 end
